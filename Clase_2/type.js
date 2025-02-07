70a00edfc7c95b169f073afded9c28114cf980a3
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-06 19:18:54 -0600

    Se añaden más casos de String, Booleanos, Arrays y sus variantes.

--- Clase_2/type.js
+++ Clase_2/type.js
@@ -4,3 +4,25 @@ console.log(mensaje.toUpperCase());
 
 let nombre = 'Alex';
 console.log(`Hola ${nombre}`); //En este caso se utiliza el backtick ` para poder utilizar variables dentro de una cadena
+console.log('Hola ' + nombre + ' ¿cómo estás?'); //En este caso se utiliza el + para poder utilizar variables dentro de una cadena (concatenación). Se necesita anadir un espacio entre las variables.
+console.log('Hola', nombre); //Otra manera de concatenar variables. Aquí no necesitamos añadir el espacio como en la línea anterior.
+
+//booleano
+let esMayor = false;
+console.log(esMayor && false);
+
+//Arrays
+let alumnos = ['Alex', 'Pablo', 'Pato', 'Paco'];
+console.log(alumnos[3]);
+
+//Modificar asignación
+alumnos[2] = 'Pepe';
+console.log(alumnos);
+
+//Añadir
+alumnos.push('Pepa');
+console.log(alumnos);
+
+alumnos.unshift('Pepito');
+console.log(alumnos);
+console.log(alumnos.length);
