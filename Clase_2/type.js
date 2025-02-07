3f639e4fe65c3b74216918b6c63bb27a1ccf8e38
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-06 18:55:55 -0600

    Se crea el archivo que se usó en la sesión de zoom de la clase 2.

---
+++ Clase_2/type.js
@@ -0,0 +1,6 @@
+//string
+let mensaje = 'Hola Mundo';
+console.log(mensaje.toUpperCase());
+
+let nombre = 'Alex';
+console.log(`Hola ${nombre}`); //En este caso se utiliza el backtick ` para poder utilizar variables dentro de una cadena
