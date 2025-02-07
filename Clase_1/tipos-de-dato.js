1177a8e8b04d95119f63b9158d5a1e3114b6e3a0
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-06 18:12:57 -0600

    Se crea archivo para el entregable de la clase 1.

---
+++ Clase_1/tipos-de-dato.js
@@ -0,0 +1,41 @@
+/* En este archivo se desplegarán diferentes tipos de datos en JavaScript 
+    Se estarán integrando algunos ejemplos desplegados en campus y otros que se encuentran en la documentación oficial de JavaScript
+*/
+
+// DATOS PROPUESTOS EN CAMPUS DECLARADOS COMO VARIABLES
+
+let datoUno = 42;
+let datoDos = 'Veinticinco';
+let datoTres = -666;
+let datoCuatro = true;
+let datoCinco = 0;
+let datoSeis = '';
+let datoSiete = null;
+let datoOcho = undefined;
+let datoNueve = FALSE;
+
+console.log("DATOS PROPUESTOS EN CAMPUS DECLARADOS COMO VARIABLES");
+console.log(typeof datoUno);
+console.log(typeof datoDos);
+console.log(typeof datoTres);
+console.log(typeof datoCuatro);
+console.log(typeof datoCinco);
+console.log(typeof datoSeis);
+console.log(typeof datoSiete);
+console.log(typeof datoOcho);
+console.log(typeof datoNueve);
+
+//DATOS PROPUESTOS EN CAMPUS, PERO SIN DECLARARLOS COMO VARIABLES
+
+console.log("DATOS PROPUESTOS EN CAMPUS SIN DECLARARLOS COMO VARIABLES");
+console.log(typeof 42);
+console.log(typeof 'Veinticinco');
+console.log(typeof -666);
+console.log(typeof true);
+console.log(typeof 0);
+console.log(typeof '');
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof FALSE);
+
+
