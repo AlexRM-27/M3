2fbad3cdab60a4fee9dbf9963deca8336b306447
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-08 12:52:41 -0600

    Se crea la solución del ejercicio 3 extra.

--- Clase_2/P2_ARM.js
+++ Clase_2/P2_ARM.js
@@ -40,7 +40,7 @@ switch (numeroAleatorio % 2) {
 }
 */
 //EJERCICIO 2
-
+/*
 let numeroAleatorio2 = Math.floor(Math.random() * 101);
 let esPrimo = true;
 if (numeroAleatorio2 < 2) { // Excluye 0 y 1 antes del bucle
@@ -57,4 +57,92 @@ if (esPrimo) {
     console.log(`El número ${numeroAleatorio2} es primo`);
 } else {
     console.log(`El número ${numeroAleatorio2} no es primo`);
+}
+*/
+//EJERCICIO 3
+/*Para este ejercicio asociaremos los meses con un número del 1 al 12 de la siguiente manera:
+    1 = Enero
+    2 = Febrero
+    3 = Marzo
+    4 = Abril
+    5 = Mayo
+    6 = Junio
+    7 = Julio
+    8 = Agosto
+    9 = Septiembre
+    10 = Octubre
+    11 = Noviembre
+    12 = Diciembre
+
+Además consideraremos las estaciones del año de la siguiente manera:
+    Verano: Junio, Julio y Agosto
+    Otoño: Septiembre, Octubre y Noviembre
+    Invierno: Diciembre, Enero y Febrero
+    Primavera: Marzo, Abril y Mayo
+*/
+
+let mes = Math.floor(Math.random() * 14); //línea que simula el ingreso de un mes al sistema. Se genera un número aleatorio entre 1 y 13, con el fin de probar el programa cuando se ingresa un valor que no es un mes del año.
+switch (mes) { //switch que simula el mes ingresado
+    case 1:
+        console.log('Enero');
+        break;
+    case 2:
+        console.log('Febrero');
+        break;
+    case 3: 
+        console.log('Marzo');    
+        break;
+    case 4:
+        console.log('Abril');
+        break;
+    case 5:
+        console.log('Mayo');
+        break;
+    case 6:
+        console.log('Junio');
+        break;
+    case 7:
+        console.log('Julio');
+        break;
+    case 8:
+        console.log('Agosto');
+        break;
+    case 9:
+        console.log('Septiembre');
+        break;
+    case 10:
+        console.log('Octubre');
+        break;
+    case 11:
+        console.log('Noviembre');
+        break;
+    case 12:
+        console.log('Diciembre');
+        break;
+    default:
+        console.log('Dato erróneo.');
+}
+switch (mes) { //switch que determina la estación del año
+    case 6:
+    case 7:
+    case 8:
+        console.log('Estamos en verano.');
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log('Estamos en otoño.');
+        break;
+    case 12:
+    case 1:
+    case 2:
+        console.log('Estamos en invierno.');
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log('Estamos en primavera.');
+        break;
+    default:
+        console.log('Ingresa un mes del año.');
 }
