2f83b2f6f60096beee96549513c25614d4b5f956
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-08 11:52:38 -0600

    Se añade la solución del ejercicio 1 extra utilizando switch,

--- Clase_2/P2_ARM.js
+++ Clase_2/P2_ARM.js
@@ -5,7 +5,7 @@
     • Si la nota está entre 60 y 74, el estudiante aprueba con "Suficiente".
     • Si la nota es menor de 60, el estudiante no aprueba.
 */
-
+/*
 let notaEstudiante = Math.floor(Math.random() * 101); // Con esta línea simulamos el ingreso de la nota al sistema.
 console.log(`La nota es: ${notaEstudiante}`); // Así visualizamos la nota en la consola.
 if (notaEstudiante > 0 && notaEstudiante <= 100) { //Con este primer if se evita el error de que el usuario ingrese un valor fuera el rango de 0 - 100.
@@ -21,9 +21,20 @@ if (notaEstudiante > 0 && notaEstudiante <= 100) { //Con este primer if se evita
 } else {
     console.log('Ingrese un valor entre 0 (cero) y 100 (cien)');
 }
-
+*/
 /* OTROS EJERCICIOS PROPUESTOS POR EL SENSEI
     1. Crear un programa que genere un número aleatorio entre 1 y 100 y te ayude a saber si un número aleatorio es par o impar.
     2. Crear un programa que compare si un número es primo o no.
     3. Crear un programa que averigue en qué estación del año estamos una vez que le ingresemos el mes.
  */
+
+// EJERCICIO 1
+
+let numeroAleatorio = Math.floor(Math.random() * 101);
+switch (numeroAleatorio % 2) {
+    case 0:
+        console.log(`El número ${numeroAleatorio} es par`);
+        break;
+    default:
+        console.log(`El número ${numeroAleatorio} es impar`);
+}
