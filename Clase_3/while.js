d85ecc5084c436dbb2e24e5ac5107ad0fe2acc7b
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-11 19:28:21 -0600

    Corregimos sintaxis do while y agregamos sintaxis for.

--- Clase_3/while.js
+++ Clase_3/while.js
@@ -1,6 +1,14 @@
 //Sintaxis do while
-let i = 0;
+let numero = 0;
 do {
-    console.log(i);
-    i++;
-} while (i < 5);
+    console.log('Ejecuto el programa al menos una vez.');
+    numero++;
+} while (numero < 5);
+
+//sintaxis for
+let variable = 0;
+for (let i = 1; i < 10; i++) {
+    variable += i;
+}
+
+console.log(`La suma total es: ${variable}`);
