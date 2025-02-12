733ce9129a254f600342a686348c5e2c02790138
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-11 19:44:49 -0600

    Se añade ejemplo de do while y se ejecuta en un archivo html para visualizarlo en el navegador.

--- Clase_3/while.js
+++ Clase_3/while.js
@@ -12,3 +12,17 @@ for (let i = 1; i < 10; i++) {
 }
 
 console.log(`La suma total es: ${variable}`);
+
+//for EJERCICIO
+const numeros = [10, 15, 47, 80, 25];
+for (let num of numeros){
+    console.log(num);
+} 
+
+// do while
+let respuesta;
+do{
+    respuesta = parseInt(prompt('Ingrese un número menor a 20'));
+}while (respuesta < 20);
+
+alert(`El número ingresado es: ${respuesta}`);
