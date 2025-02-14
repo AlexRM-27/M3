74196113419891925156d24b029010d407cd8d66
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-13 19:45:12 -0600

    Se añade ejemplo de multiplicar un array.

--- Clase_4/arrow.js
+++ Clase_4/arrow.js
@@ -6,3 +6,8 @@ const saludar = (nombre) => {
     return `Hola ${nombre}`
 }
 console.log(saludar('Alex'));
+
+
+const numeros = [1,2,3,4,5,6,7,8,10]
+const multiplicados = numeros.map(num => num * 8)
+console.log(multiplicados);
