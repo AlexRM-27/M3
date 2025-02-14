2cfaaedb2cf0251bcb9977ce69c7f439b2e4e65c
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-13 19:11:05 -0600

    Se añaden sintaxis de funciones en Js

---
+++ Clase_4/type.js
@@ -0,0 +1,11 @@
+//Función sin parámetro
+function saludar() {
+    console.log('Hola mundo');
+}
+saludar(); //mandamos a llamar a la función
+
+//Función con parámetros
+function saludarAlguien(nombre) {
+    console.log(`Hola ${nombre}`);
+}
+saludarAlguien('Alex');
