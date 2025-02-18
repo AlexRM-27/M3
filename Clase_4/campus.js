bb8eef3cfa1d217ea2d06a2ab8688bdd653196af
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-17 20:36:23 -0600

    Se añade ejercicio de conversión de temperatura.

--- Clase_4/campus.js
+++ Clase_4/campus.js
@@ -25,3 +25,18 @@ console.log("Suma: " + sumar(2, 3));
 console.log("Resta: " + restar(5, 2));
 console.log("Multiplicación: " + multiplicar(4, 6));
 console.log("División: " + dividir(10, 2));
+
+/* EJERCICIO 2: CONVERSOR DE TEMPERATURAS
+Descripción: crea una función que convierta tamperaturas entre grados Celsius y Fahrenheit.
+*/
+
+function celsiusAFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+}
+
+function fahrenheitACelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5/9;
+}
+
+console.log("20°C en Fahrenheit es: " + celsiusAFahrenheit(20) + "°F");
+console.log("68°F en Celsius es: " + fahrenheitACelsius(68) + "°C");
