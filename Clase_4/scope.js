ffa50ef1f47c9718c7d3d37d2d5e800187e86a0d
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-17 22:27:31 -0600

    Se añaden más ejemplos de block scope y declaraciones de ejercicios propuestos por el sensei.

--- Clase_4/scope.js
+++ Clase_4/scope.js
@@ -14,3 +14,23 @@ function saludo(){
 }
 
 saludo();
+
+//Block scope
+if(true){
+    let blockScope = "Solo existe este bloque" // solo utilizamos let para que nos aseguremos de que esta variable solo exista dentro de la función.
+    console.log(blockScope);
+}
+
+console.log(blockScope);
+
+
+/* EJERCICIOS PROPUESTOS POR EL SENSEI
+E1: Determinar si una palabra es un palindromo
+
+E2: filtra los números mayores a un valor dado
+
+E3: obtener el área de un círculo
+
+E4: de los ejercicios anteriores, rehacerlo usando con función
+
+*/
