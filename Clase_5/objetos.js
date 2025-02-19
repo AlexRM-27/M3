23e233b4c452298eaad472584e50547024a32cd9
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-18 19:26:36 -0600

    Se crea archivo para la clase 5.

---
+++ Clase_5/objetos.js
@@ -0,0 +1,29 @@
+// estructura base de un objeto simple
+const persona = {
+    nombre: 'Alejandro', // clave: valor
+    edad: 25,
+    ciudad: 'Cedemequis'
+}
+
+// Modificación de valores
+persona.ciudad = 'Cordoba'; // se cambia la ciudad de la persona
+
+//Añadir valores
+persona.genero = 'Masculino';
+persona.estatura = 1.75;
+
+delete.persona.estatura; // se elimina la propiedad estatura
+
+
+// Objetos con métodos
+const coche = {
+    marca: 'Chevrolet',
+    modelo: 'Aveo 2025',
+    color: 'gris',
+
+    arrancar: function(marca) { //esto es un método, es decir una acción que está haciendo el objeto; se pueden añadir parámetros
+        console.log('El coche está arrancando', marca);
+    }
+}
+
+coche.arrancar(coche.marca);
