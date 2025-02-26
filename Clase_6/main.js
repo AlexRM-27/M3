dc5e0fde4f63b4f0c92d8e98f5eaf67b49d4742c
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-25 18:34:39 -0600

    Se añaden párrafos desde Js al HTML y se genera un botón con funcionalidad.

--- Clase_6/main.js
+++ Clase_6/main.js
@@ -5,7 +5,33 @@ let titulo = document.getElementById('titulo');
 titulo.innerHTML = 'Alex y sus Extremitas';
 titulo.style.color = 'blue';
 
-let nuevoParrafo = document.createElement('p');
-nuevoParrafo.innerHTML = 'Bienvenidos a la clase 6';
+let nuevoParrafo = document.createElement('p'); // Agregamos un párrafo desde el DOM
+nuevoParrafo.innerHTML = 'Bienvenidos a la clase 6'; // Agregamos contenido al parágrafo
 
-document.body.appendChild(nuevoParrafo);
+document.body.appendChild(nuevoParrafo); // Agregamos el parágrafo al body
+
+let nuevoParrafo2 = document.createElement('p'); // Simulamos agregar un parágrafo
+nuevoParrafo2.innerHTML = 'Bienvenidos a la clase 6'; // Agregamos contenido al parágrafo
+
+document.body.insertBefore(nuevoParrafo2, titulo); // Se añade el párrafo antes del título
+
+/*
+nuevoParrafo2.remove(); // Elimina el parrafo
+document.body.removeChild(nuevoParrafo2);
+*/
+
+function retraso() {
+    setTimeout(() => {
+        let parrafoSecundario = document.createElement('p')
+        parrafoSecundario.innerHTML = 'Voy a aparecer 3 segundos después'
+        document.body.appendChild(parrafoSecundario)
+    }, 3000)
+}
+
+retraso();
+
+function agregarElementos() {
+    let nuevoElemento = document.createElement('p')
+    nuevoElemento.innerHTML = 'Leidis and madafakas'
+    document.body.appendChild(nuevoElemento)
+}
