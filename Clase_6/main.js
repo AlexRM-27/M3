0d13f6fc16b49e780f7cea8c7e2a2ffd1cfe5686
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-25 18:17:18 -0600

    Primeras interacciones del DOM.

--- Clase_6/main.js
+++ Clase_6/main.js
@@ -1,1 +1,11 @@
-document.getElementById('parrafo').innerHTML = 'Hola Mundo';
+console.log (document.documentElement); // Accediendo al nodo HTML
+console.log (document.body); // Accediendo al nodo body
+
+let titulo = document.getElementById('titulo');
+titulo.innerHTML = 'Alex y sus Extremitas';
+titulo.style.color = 'blue';
+
+let nuevoParrafo = document.createElement('p');
+nuevoParrafo.innerHTML = 'Bienvenidos a la clase 6';
+
+document.body.appendChild(nuevoParrafo);
