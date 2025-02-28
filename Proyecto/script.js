1bb38b490846a8f7943fca54a91c03a056a6cb7b
AlexRM-27 <alejandro.ruiz@ingenieria.unam.edu>
2025-02-27 19:11:38 -0600

    Se añaden más usuarios a la base de datos y se modifica página de home.

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,14 +1,19 @@
-const alexUser = "alex";
-const alexPassword = "1234";
+const users = [
+    {name: "Alejandro Ruiz", username: "AlexRM", password: "1234", image: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600"},
+    {name: "Florencia Mendoza", username: "Florens9699", password: "abcd", image: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"},
+    {name: "Ameyalli Vanessa Ruiz", username: "AmeVRM", password: "qwer", image: "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=600"},
+]
 
 function logIn() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const usernamePage = document.getElementById('username').value.trim();
+    const passwordPage = document.getElementById('password').value.trim();
     const error = document.getElementById('error');
 
-    if (username === alexUser && password === alexPassword) {
-        localStorage.setItem("loggedUser", username);
-        window.location.href = "home.html"
+    const userFind = users.find(user => user.username === usernamePage && user.password === passwordPage);
+
+    if (userFind) {
+        localStorage.setItem("loggedUser", JSON.stringify(user));
+        window.location.href = "home.html";
     } else {
         error.textContent = 'Credenciales incorrectas'
     }
